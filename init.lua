63132c358a546432dbeed5e0d12d5be07fec6473
John Persson <john.persson@polestar.com>
2023-05-27 23:10:47 +0200

    format file

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
-require "bira.settings"
-require "bira.plugins-setup"
-require "bira.keymap"
+require("bira.settings")
+require("bira.plugins-setup")
+require("bira.keymap")
 -- in order for plugin keymaps to take precedence, plugins need to be required after keymaps
-require "bira.plugins"
-require "bira.lsp"
+require("bira.plugins")
+require("bira.lsp")
 
 -- Enable Comment.nvim
-require('Comment').setup()
-
+require("Comment").setup()
