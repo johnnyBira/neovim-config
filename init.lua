c62ae21b77c25d6cca254064342974aa3a771b50
John Persson <john.persson@polestar.com>
2023-06-01 23:12:53 +0200

    correct init order

--- init.lua
+++ init.lua
@@ -1,9 +1,6 @@
-require("bira.settings")
 require("bira.plugins-setup")
 require("bira.keymap")
 -- in order for plugin keymaps to take precedence, plugins need to be required after keymaps
 require("bira.plugins")
+require("bira.settings")
 require("bira.lsp")
-
--- Enable Comment.nvim
-require("Comment").setup()
