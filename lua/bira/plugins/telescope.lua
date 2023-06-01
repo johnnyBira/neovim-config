789a8383e21015ab03b0dac1906cb09f044d6d2b
John Persson <john.persson@polestar.com>
2023-06-01 23:53:09 +0200

    additional telescope keymaps for git and keymaps

--- lua/bira/plugins/telescope.lua
+++ lua/bira/plugins/telescope.lua
@@ -41,7 +41,6 @@ end, { desc = "[/] Fuzzily search in current buffer]" })
 vim.keymap.set("n", "<leader>sc", function()
 	-- You can pass additional configuration to telescope to change theme, layout, etc.
 	require("telescope.builtin").find_files({
-		-- titkejj
 		cwd = "~/.config/nvim",
 		prompt_title = "[S]earch [C]onfigs]",
 	})
@@ -49,6 +48,9 @@ end, { desc = "[S]earch [C]onfigs" })
 
 vim.keymap.set("n", "<leader>sf", require("telescope.builtin").find_files, { desc = "[S]earch [F]iles" })
 vim.keymap.set("n", "<leader>sh", require("telescope.builtin").help_tags, { desc = "[S]earch [H]elp" })
+vim.keymap.set("n", "<leader>sl", require("telescope.builtin").git_status, { desc = "[S]earch Git [L]og" })
+vim.keymap.set("n", "<leader>sl", require("telescope.builtin").git_branches, { desc = "[S]earch Git [B]ranches" })
+vim.keymap.set("n", "<leader>sk", require("telescope.builtin").keymaps, { desc = "[S]earch [K]eymaps" })
 vim.keymap.set("n", "<leader>sw", require("telescope.builtin").grep_string, { desc = "[S]earch current [W]ord" })
 vim.keymap.set("n", "<leader>sg", require("telescope.builtin").live_grep, { desc = "[S]earch by [G]rep" })
 vim.keymap.set("n", "<leader>sd", require("telescope.builtin").diagnostics, { desc = "[S]earch [D]iagnostics" })
