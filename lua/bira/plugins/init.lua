be1efb823bf99eccabecb85f1a46297a6cab72a5
John Persson <john.persson@polestar.com>
2023-12-18 00:04:48 +0100

    cleanup rose-pine plugin config

--- lua/bira/plugins/init.lua
+++ lua/bira/plugins/init.lua
@@ -6,13 +6,6 @@ return {
 	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
 	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = false },
 	{ "catppuccin/nvim", name = "catppuccin", event = "VeryLazy" },
-	{
-		"rose-pine/neovim",
-		as = "rose-pine",
-		config = function()
-			require("rose-pine").setup()
-			-- vim.cmd('colorscheme rose-pine')
-		end,
-	},
+	{ "rose-pine/neovim", as = "rose-pine", event = "VeryLazy" },
 	{ "tpope/vim-rhubarb", lazy = true, cmd = { "Gbrowse", "Gpull", "Gpush" } },
 }
