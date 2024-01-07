6093b0e060f165160c8ad0ad9afcaab4d7351b7b
John Persson <john.persson@polestar.com>
2024-01-07 14:50:41 +0100

    change keymap fo harpoon nav_prev

--- lua/bira/plugins/harpoon.lua
+++ lua/bira/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
 		vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "[H]arpoon [A]dd", noremap = true })
 		vim.keymap.set("n", "<leader>hd", mark.rm_file, { desc = "[H]arpoon [D]elete", noremap = true })
 		vim.keymap.set("n", "<leader>hn", ui.nav_next, { desc = "[H]arpoon [N]ext", noremap = true })
-		vim.keymap.set("n", "<leader>hp", ui.nav_prev, { desc = "[H]arpoon [P]revious", noremap = true })
+		vim.keymap.set("n", "<leader>hN", ui.nav_prev, { desc = "[H]arpoon [P]revious", noremap = true })
 		-- setup mappings for navigation between marks 1-9
 		for i = 1, 9 do
 			vim.keymap.set("n", "<leader>h" .. i, function()
