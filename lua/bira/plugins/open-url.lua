775f91f6d7e1e69cac4c34c9b1220f7ce76eb751
John Persson <john.persson@polestar.com>
2024-04-28 22:56:07 +0200

    add plugin for opening urls

---
+++ lua/bira/plugins/open-url.lua
@@ -0,0 +1,12 @@
+return {
+	"sontungexpt/url-open",
+	keys = { "<CR>" },
+	config = function()
+		local status_ok, url_open = pcall(require, "url-open")
+		if not status_ok then
+			return
+		end
+		url_open.setup({})
+		vim.keymap.set("n", "<CR>", "<ESC>:URLOpenUnderCursor<CR>", { silent = true })
+	end,
+}
