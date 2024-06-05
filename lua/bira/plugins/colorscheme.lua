4c0af4d8ea22d1dff73cbf00cc4cec4f9f4e534f
John Persson <john.persson@polestar.com>
2024-06-05 23:09:54 +0200

    set nightfox background color to transparent

--- lua/bira/plugins/colorscheme.lua
+++ lua/bira/plugins/colorscheme.lua
@@ -2,6 +2,10 @@ return {
 	"EdenEast/nightfox.nvim",
 	priority = 1000,
 	config = function()
+		require("nightfox").setup({
+			options = { transparent = true },
+		})
+
 		vim.cmd("colorscheme duskfox")
 	end,
 }
