23136140e9b4b0833e72a2b4621a37164e3d1060
John Persson <john.persson@polestar.com>
2024-06-05 22:58:34 +0200

    remove catppussin colorscheme

--- lua/bira/plugins/init.lua
+++ lua/bira/plugins/init.lua
@@ -2,7 +2,6 @@ return {
 	"tpope/vim-sleuth",
 	-- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
 	{ "nvim-telescope/telescope-fzf-native.nvim", build = "make", cond = false },
-	{ "catppuccin/nvim", name = "catppuccin", event = "VeryLazy" },
 	{ "rose-pine/neovim", as = "rose-pine", event = "VeryLazy" },
 	{ "tpope/vim-rhubarb", lazy = true, cmd = { "Gbrowse", "Gpull", "Gpush" } },
 }
