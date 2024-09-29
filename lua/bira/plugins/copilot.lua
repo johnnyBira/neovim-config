61d02765c6913e8474f1b555cc1a46c2f0caae15
John Persson <john.persson@polestar.com>
2024-09-29 23:34:17 +0200

    update copilot keybindings

--- lua/bira/plugins/copilot.lua
+++ lua/bira/plugins/copilot.lua
@@ -1,11 +1,15 @@
 return {
 	"github/copilot.vim",
 	event = "BufRead",
-	-- config = function()
-	-- 	require("copilot").setup({
-	-- 		suggestion = { enabled = true },
-	-- 		-- panel = { enabled = false }
-	-- 	})
-	-- vim.cmd("autocmd BufEnter * CopilotStart")
-	-- end,
+	config = function()
+		-- I had some problems with tmux not sending the correct keycodes for Ctrl+Enter
+		-- so I had to change the keybinding to use the Enter key instead
+		vim.keymap.set("i", "‰", 'copilot#Accept("\\<CR>")', {
+			expr = true,
+			replace_keycodes = false,
+		})
+		vim.g.copilot_no_tab_map = true
+		vim.keymap.set("i", "<C-l>", "<Plug>(copilot-next)")
+		vim.keymap.set("i", "<C-h>", "<Plug>(copilot-previous)")
+	end,
 }
